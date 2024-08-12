b3f79fbfa2cb5a97d8e487cffd71f94ffaa5beff
Nicolas <devnicolas.coroa@gmail.com>
2024-08-11 21:17:52 -0300

    :construction: Save progress to implements search user

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,40 @@
 import HelperFetch from "./helper/data-helper.js"
 import { Table } from "./view/table.js"
 import ButtonsHandlers from "./handlers/home/buttons.js"
-import { init } from "./handlers/home/search.js"
 
 window.apiUrl = "http://localhost:3000"
 
 document.addEventListener("DOMContentLoaded", async function () {
+  const mainTable = new Table("[data-table-main]", [])
   try {
     const helperFetch = new HelperFetch(window.apiUrl)
     const allUsers = await helperFetch.getAllCustomers("/clientes")
-    const mainTable = new Table("[data-table-main]", allUsers)
+    window.localStorage.setItem("allUsers", JSON.stringify(allUsers.reverse()))
+    mainTable.customerList = allUsers.reverse()
     mainTable.render()
     ButtonsHandlers.init()
-    init()
   } catch (error) {
     console.error("Erro:", error)
   }
+
+  const formSearch = document.querySelector("form[data-search-form]")
+  formSearch.addEventListener("submit", e => {
+    e.preventDefault()
+    const searchTerm = e.target.querySelector("input.search-bar").value
+    
+    if (!searchTerm.trim().length) {
+      return
+    }
+
+    const users = JSON.parse(window.localStorage.getItem("allUsers"))
+    const usersSearch = users.filter(user => user.nome.toLowerCase().includes(searchTerm))
+    mainTable.customerList = usersSearch
+    mainTable.render()
+  })
+
+  formSearch.querySelector("input.search-bar").addEventListener("search", (e) => {
+    console.log(e)
+    mainTable.customerList = JSON.parse(window.localStorage.getItem("allUsers"))
+    mainTable.render()
+  })
 })
