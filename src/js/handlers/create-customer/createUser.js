8e4239d0f5ea0bf024f20efc0c0e5ab65bf9e637
Nicolas Soares <87401887+d3vnicolas@users.noreply.github.com>
2024-08-17 21:51:04 -0300

    Update createUser.js

--- src/js/handlers/create-customer/createUser.js
+++ src/js/handlers/create-customer/createUser.js
@@ -4,21 +4,22 @@ import HelperFetch from "../../helper/data-helper.js"
 const BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? '/' : '/crud-frontend/'
 
 const form = document.querySelector("[data-form]")
-
-form.addEventListener("submit", async e => {
-  e.preventDefault()
-  const dataObj = Array.from(e.target.querySelectorAll("input, select")).reduce((obj, field) => {
-    obj[field.name] = field.value
-    return obj
-  }, {})
+document.addEventListener("DOMContentLoaded", async function () {
+  form.addEventListener("submit", async e => {
+    e.preventDefault()
+    const dataObj = Array.from(e.target.querySelectorAll("input, select")).reduce((obj, field) => {
+      obj[field.name] = field.value
+      return obj
+    }, {})
+    
+    const helperFetch = new HelperFetch("https://crud-api-bg41.onrender.com")
+    helperFetch.addCustomer(dataObj).then(() => {
+      alert("Cliente cadastrado com sucesso.")
+      window.location.assign(BASE_URL)
+    })
+  })
   
-  const helperFetch = new HelperFetch("https://crud-api-bg41.onrender.com")
-  helperFetch.addCustomer(dataObj).then(() => {
-    alert("Cliente cadastrado com sucesso.")
+  document.querySelector("[data-back-page]").addEventListener("click", () => {
     window.location.assign(BASE_URL)
   })
-})
-
-document.querySelector("[data-back-page]").addEventListener("click", () => {
-  window.location.assign(BASE_URL)
-})
+}
