37b2e8d675d3f52c621f7c9aba8f08ae2e82978a
Nicolas <devnicolas.coroa@gmail.com>
2024-12-26 19:34:11 -0300

    :tada: Begin React with Next project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  {
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  pluginJs.configs.recommended,
-  {
-    rules: {
-      semi: ["error", "never"],
-    },
-  },
-]
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
